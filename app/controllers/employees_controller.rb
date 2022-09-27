d93853ad12e17ad91d2fe3d2aa04babe5df73686
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-09-27 12:00:23 -0600

    test: only had time to add test for user extension

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -2,12 +2,4 @@ class EmployeesController < ApplicationController
   def show
     @employee = Employee.find(params[:id])
   end
-
-
-
-
-  # private
-  # def emp_params
-  #   params.permit(:subject)
-  # end
 end
--- spec/features/departments/index_spec.rb
+++ spec/features/departments/index_spec.rb
@@ -9,18 +9,19 @@ RSpec.describe 'When I visit the Departments#index page' do
     @employee_2 = @dept_1.employees.create!(name: "Bryan L", level: 3)
     @employee_3 = @dept_2.employees.create!(name: "Miranda S", level: 4)
 
+    @ticket_1 = @employee_1.tickets.create!(subject: "Forgot my password again", age: 1, open: true)
+    @ticket_2 = @employee_1.tickets.create!(subject: "Would love a fourth monitor", age: 4, open: false)
+    @ticket_3 = @employee_2.tickets.create!(subject: "Printer not working", age: 3, open: false)
+    @ticket_4 = @employee_2.tickets.create!(subject: "Mouse Double Clicking", age: 5, open: false)
+    @ticket_5 = @employee_3.tickets.create!(subject: "Keybrd buttns not wrkng", age: 2, open: false)
+    @ticket_6 = @employee_3.tickets.create!(subject: "Monitor wont turn on cant see", age: 6, open: false)
+    @ticket_7 = @employee_3.tickets.create!(subject: "Can't turn on computer now", age: 5, open: false)
+
+
   end
 
-  # Story 1
-# Department Index
-
-# As a user,
-# When I visit the Department index page,
-# I see each department's name and floor
-# And underneath each department, I can see the names of all of its employees
-
   it 'shows each departments name and floor, along with a list of each depts employees (US#1)' do
-    visit departments_path #update once routes are made
+    visit departments_path 
     within("#dept-#{@dept_1.id}") do
       expect(page).to have_content(@dept_1.name)
       expect(page).to have_content(@dept_1.floor)
@@ -32,6 +33,24 @@ RSpec.describe 'When I visit the Departments#index page' do
       expect(page).to have_content(@dept_2.floor)
       expect(page).to have_content(@employee_3.name)
     end
+  end
+# Extension
+# Top Employees
 
+# As a user,
+# When I visit the department index page,
+# Then I see a section for "Top Employees",
+# And in that section I see the names of the 3 employees
+# who have the highest count of closed tickets, 
+# and next to the employee name I see their count of closed tickets and their department
+  xit 'shows a section for Top Employees that shows the names of the 3 employees with the highest count of closed tickets with the num of closed tickets next to their name' do #emp 3, 2, 1
+    visit departments_path
+
+    expect(@employee_3).to appear_before(@employee_2)
+    expect(@employee_2).to appear_before(@employee_1)
+
+    expect(page).to have_content("#{@employee_3.name} has 3 closed tickets")
+    expect(page).to have_content("#{@employee_2.name} has 2 closed tickets")
+    expect(page).to have_content("#{@employee_1.name} has 1 closed tickets")
   end
 end
